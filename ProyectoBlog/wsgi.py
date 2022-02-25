900121530b04596acd1b1f7fe1a54e68b4e4361b
gusgeneris <gusgeneris92@gmail.com>
2022-02-25 01:32:25 -0300

    Heroku

--- ProyectoBlog/wsgi.py
+++ ProyectoBlog/wsgi.py
@@ -15,5 +15,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProyectoBlog.settings')
 
 application = get_wsgi_application()
 
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
