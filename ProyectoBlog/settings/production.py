d6fb9a7b3576d0fb6162fd7efa700cf1d469b1c1
gusgeneris <gusgeneris92@gmail.com>
2022-02-25 00:00:40 -0300

    Heroku

--- ProyectoBlog/settings/production.py
+++ ProyectoBlog/settings/production.py
@@ -10,8 +10,8 @@ ALLOWED_HOSTS = ['django-generis-blog.herokuapp.com']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd3kmclk843v4k8',
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME':'d3kmclk843v4k8',
         'USER':'pukdrsdgrzhyps',
         'PASSWORD':'2e829bd0a24524633a3803c2db4b9935f15af1afd55aeb4a4e19f3707d879dd3',
         'HOST':'ec2-54-209-221-231.compute-1.amazonaws.com',
